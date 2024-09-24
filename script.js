c7a7332a784a70308f8c377aa06b57d61a1f0a1f
Tom <tom@device-143.home>
2024-09-24 17:19:11 +0200

    update

--- script.js
+++ script.js
@@ -172,31 +172,54 @@ function createPasswordZone() {
   zoneInput.classList.add("zone-input");
 
   const backButton = document.createElement("button");
-  const backButtonSVG = document.createElement("img");
-  backButtonSVG.src = "/assets/arrow.svg";
-  backButtonSVG.alt = "fleche retour";
-  backButtonSVG.classList.add("back-arrow");
   backButton.classList.add("back-button", "password-button");
 
+  // Contenu SVG pour le bouton de retour, avec une taille de 36x36
+  const backButtonSVG = `
+    <svg xmlns="http://www.w3.org/2000/svg" height="36" viewBox="0 0 24 24" width="36">
+      <path d="M14.71 15.88L10.83 12l3.88-3.88c.39-.39.39-1.02 0-1.41-.39-.39-1.02-.39-1.41 0L8.71 11.3c-.39.39-.39 1.02 0 1.41l4.59 4.59c.39.39 1.02.39 1.41 0 .38-.39.39-1.03 0-1.42z"/>
+    </svg>
+  `;
+  backButton.innerHTML = backButtonSVG;
+
   const copyButton = document.createElement("button");
-  const copyButtonSVG = document.createElement("img");
-  copyButtonSVG.src = "/assets/copy.svg";
-  copyButtonSVG.alt = "fleche retour";
-  copyButtonSVG.classList.add("copy-icon");
   copyButton.classList.add("copy-button", "password-button");
 
+  // Contenu SVG pour le bouton de copie
+  const copyButtonSVG = `
+    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+      <path d="M0 0h24v24H0V0z" fill="none"/>
+      <path d="M15 1H4c-1.1 0-2 .9-2 2v13c0 .55.45 1 1 1s1-.45 1-1V4c0-.55.45-1 1-1h10c.55 0 1-.45 1-1s-.45-1-1-1zm.59 4.59l4.83 4.83c.37.37.58.88.58 1.41V21c0 1.1-.9 2-2 2H7.99C6.89 23 6 22.1 6 21l.01-14c0-1.1.89-2 1.99-2h6.17c.53 0 1.04.21 1.42.59zM15 12h4.5L14 6.5V11c0 .55.45 1 1 1z"/>
+    </svg>
+  `;
+  copyButton.innerHTML = copyButtonSVG;
+
   passwordContainer.appendChild(backButton);
-  backButton.appendChild(backButtonSVG);
   passwordContainer.appendChild(labelPassword);
   passwordContainer.appendChild(zoneInput);
   zoneInput.appendChild(inputPassword);
   zoneInput.appendChild(copyButton);
-  copyButton.appendChild(copyButtonSVG);
 
   backButton.addEventListener("click", () => {
     passwordContainer.innerHTML = "";
     displayPasswordZone();
   });
+
+  copyButton.addEventListener("click", () => {
+    navigator.clipboard
+      .writeText(inputPassword.value)
+      .then(() => {
+        alert("Mot de passe copié dans le presse-papiers !");
+      })
+      .catch((err) => {
+        console.error("Erreur lors de la copie du mot de passe : ", err);
+      });
+  });
+
+  console.log("input password :", inputPassword);
+  console.log("password :", password);
+  inputPassword.innerHTML = "";
+  inputPassword.setAttribute("value", password);
 }
 
 function displayPasswordZone() {
